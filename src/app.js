a5a2cc1c092ab0a8f925c1424be383bbaaaa72f7
Joshua Kleveter <joshua.kleveter@gmail.com>
2015-12-28 18:30:59 -0600

    Wrote psudeo-code for app

---
+++ src/app.js
@@ -0,0 +1,8 @@
+//Get a Treehouse user's badge count and points in each category from their profile
+//Provide option to see total points of a single category
+//
+//Get username(s) and optional category arg from CLI
+//Get user profile(s) from Treehouse
+//Parse profile data
+//Format data
+//Output to CLI
