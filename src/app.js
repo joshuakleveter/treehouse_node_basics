3708ef5560974df8287d8a8da31f130f891a3e15
Joshua Kleveter <joshua.kleveter@gmail.com>
2015-12-28 18:55:47 -0600

    Created get-profile.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 //Get a Treehouse user's badge count and points in each category from their profile
 //Provide option to see total points of a single category
-//
+
+////////////
+//Imports //
+////////////
+
+import getProfile from "./get-profile.js";
+
 //Get username(s) and optional category arg from CLI
 //Get user profile(s) from Treehouse
 //Parse profile data
