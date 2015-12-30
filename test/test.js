551b2dee4a5651cb65086b4c00e7dad1267cb32b
Joshua Kleveter <joshua.kleveter@gmail.com>
2015-12-29 18:09:27 -0600

    Wrote test for get-profile public API

--- test/test.js
+++ test/test.js
@@ -8,3 +8,17 @@
 //Imports and Global Vars
 var chai = require("chai");
 var expect = chai.expect;
+
+
+
+////////////////
+//Mocha Tests //
+////////////////
+
+describe("get-profile module", () => {
+    context("public API", () => {
+        it("should be an object", () => {
+            expect(getProfile()).to.be.an("object");
+        });
+    });
+});
