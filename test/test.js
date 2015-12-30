39e9da52a86774719ae7cdacc690039e618fd564
Joshua Kleveter <joshua.kleveter@gmail.com>
2015-12-29 18:00:20 -0600

    Added DocBlocks

--- test/test.js
+++ test/test.js
@@ -1,4 +1,10 @@
+//////////////////
+//Configuration //
+//////////////////
+
+//ESLint config
 /*eslint-env mocha*/
 
+//Imports and Global Vars
 var chai = require("chai");
 var expect = chai.expect;
