2b9722545944e81c998bc8e849b6be4f27e177b8
Joshua Kleveter <joshua.kleveter@gmail.com>
2015-12-29 18:33:00 -0600

    Added doc blocks

--- src/get-profile.js
+++ src/get-profile.js
@@ -2,8 +2,17 @@
 //Get-Profile Module //
 ///////////////////////
 
+/**
+ * Return the module's publicAPI to the 'api' object.
+ * @return {Object} - publicAPI
+ */
 var api = (function getProfile() {
 
+    /**
+     * Public API object.
+     * This references any public functions of the module.
+     * @type {Object}
+     */
     var publicAPI = {
         //public function references go here.
     };
