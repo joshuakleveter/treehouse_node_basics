12627f3b3f09b3a579f3a1b31b38297f3f630b7a
Joshua Kleveter <joshua.kleveter@gmail.com>
2015-12-29 18:49:23 -0600

    Wrote tests for getArguments() function

--- test/test.js
+++ test/test.js
@@ -17,9 +17,28 @@ var getProfile = require("../dist/get-profile.js");
 ////////////////
 
 describe("get-profile module", () => {
+    'use strict';
+
     context("public API", () => {
         it("should be an object", () => {
             expect(getProfile.api).to.be.an("object");
         });
     });
+
+    context("getArguments() function", () => {
+        var args = getProfile.api.getArguments();
+        it("should return an object", () => {
+            expect(args).to.be.an("object");
+        });
+        it("should return a 'users' property that is an array with a minimum length of 1", () => {
+            expect(args).to.have.property("users");
+            expect(args.users).to.be.an("array");
+            expect(args.users.length).to.be.above(0);
+        });
+        it("should return a 'topic' property that is a string", () => {
+            expect(args).to.have.property("topic");
+            expect(args.topic).to.be.a("string");
+        });
+    });
+
 });
