2a3fa3396fadf98a0e47c6616770091419c8c476
Joshua Kleveter <joshua.kleveter@gmail.com>
2015-12-29 18:34:00 -0600

    Moved app.js psudeo-code to get-profile.js

--- src/get-profile.js
+++ src/get-profile.js
@@ -2,6 +2,19 @@
 //Get-Profile Module //
 ///////////////////////
 
+//Get a Treehouse user's badge count and points in each category from their profile
+//Provide option to see total points of a single category
+
+
+
+//Get username(s) and optional category arg from CLI
+//Get user profile(s) from Treehouse
+//Parse profile data
+//Format data
+//Output to CLI
+
+
+
 /**
  * Return the module's publicAPI to the 'api' object.
  * @return {Object} - publicAPI
