fc7adb57a7319a61f2667b4188920d34fcd8c210
Joshua Kleveter <joshua.kleveter@gmail.com>
2015-12-30 20:05:05 -0600

    Added Yargs import to app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+////////////
+//Imports //
+////////////
 
+var argv = require("yargs")
+            .alias("h", "help")
+            .alias("t", "topic")
+            .demand(1)
+            .describe("t", "Show total points in a single topic. (--topic [topic name])")
+            .help("h")
+            .usage("Usage: $0 [username(s)] [options]")
+            .argv;
+
+
+
+/////////////
+//App Code //
+/////////////
